dc32d4b8d1dd938a7678127797bdf4560e1f526d
geebski <mlabenski123@gmail.com>
2023-05-26 21:28:18 -0400

    Update vote-bar.vue

--- src/components/game-room/vote-bar.vue
+++ src/components/game-room/vote-bar.vue
@@ -1,53 +1,61 @@
 <template>
-  <v-sheet :color="voteColor" elevation="1" height="65px"></v-sheet>
+      <v-sheet v-if="currentPlayerNumberOfVotes>0" color="red" elevation="1" height="65px" >
+      </v-sheet>
+      <v-sheet v-if="currentPlayerNumberOfVotes===0" color="white" elevation="1" height="65px" >
+      </v-sheet>
+    <v-sheet v-if="currentPlayerNumberOfVotes<0" color="green" elevation="1" height="65px" >
+    </v-sheet>
 </template>
 
 <script>
 export default {
-  props: ['currentPlayerNumberOfVotes'],
-  
+  props: [
+    'currentPlayerNumberOfVotes'
+  ],
   data() {
     return {
-      whiteWidthRedSide: '',
-      redWidth: '',
-      greenWidth: '',
-      whiteWidthGreenSide: ''
-    };
-  },
-  
-  computed: {
-    voteColor() {
-      if (this.currentPlayerNumberOfVotes > 0) return "red";
-      else if (this.currentPlayerNumberOfVotes < 0) return "green";
-      else return "white";
+      
+        whiteWidthRedSide: '',
+        redWidth: '',
+        greenWidth: '',
+        whiteWidthGreenSide: ''
     }
   },
-  
   methods: {
-    calculateVoteBar(votes) {
+    //this is where the calculation for width will take place:
+    calculateVoteBar(votes){
       var votesPositive = Math.abs(votes);
       var redWidthCalculation = votesPositive * 10;
-      var whitePercent = 50 - redWidthCalculation;
-      return { redPercent: redWidthCalculation - 5, whitePercent: whitePercent - 5 };
+      var redPercent = redWidthCalculation.toString();
+      var whitePercent = 50-redWidthCalculation;
+      var whitePercentString = whitePercent.toString();
+      console.log(redPercent, whitePercent);
+      //so whitepercent is passed as 50%
+      return [redPercent-5, whitePercentString-5];
     }
+    
   },
-  
   watch: {
-    currentPlayerNumberOfVotes(val) {
+    currentPlayerNumberOfVotes (val) {
       var numbers = this.calculateVoteBar(val);
+      console.log(numbers);
       if(val < 0) {
-        this.redWidth = `${numbers.redPercent}%`;
-        this.whiteWidthRedSide = `${numbers.whitePercent}%`;
+        //make them red bars
+        this.redWidth = numbers[0]+"%";
+        this.whiteWidthRedSide = numbers[1]+"%";
         this.whiteWidthGreenSide = '50%';
-      } else if (val > 0) {
-        this.greenWidth = `${numbers.redPercent}%`;
-        this.whiteWidthGreenSide = `${numbers.whitePercent}%`;
-        this.whiteWidthRedSide = '50%';
-      } else {
+      }
+      else if (val > 0) {
+        this.greenWidth = numbers[0]+"%";
+        this.whiteWidthGreenSide = numbers[1]+"%";
+        this.whiteWidthRedSide = '50%'
+      }
+      else {
         this.greenWidth = '0%';
         this.whiteWidthGreenSide='50%';
         this.redWidth = '0%';
         this.whiteWidthRedSide = '50%';
+        //the votes are in the middle
       }
     }
   }
